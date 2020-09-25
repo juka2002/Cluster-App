65b28e569e351a548a25600922069119b8cbbba1
juka2002 <juan_bellido@persiste.com.pe>
2020-09-25 10:18:34 -0500

    Debug False static 2

--- ClusterApp/settings.py
+++ ClusterApp/settings.py
@@ -132,8 +132,9 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # esto desactiva el error de sync to async
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
