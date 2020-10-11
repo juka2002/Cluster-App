b8abd981f54e778a1f5c6df696f0f67f7721b257
juka2002 <juan_bellido@persiste.com.pe>
2020-10-11 14:24:44 -0500

    Final 2

--- ClusterApp/settings.py
+++ ClusterApp/settings.py
@@ -159,8 +159,12 @@ django_heroku.settings(locals())
 
 #jwt
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
 }
 
