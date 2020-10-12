1d31f4891111a73e1cd478be13eb89726c385d5d
juka2002 <juan_bellido@persiste.com.pe>
2020-10-11 20:22:25 -0500

    Autentificacion final

--- ClusterApp/settings.py
+++ ClusterApp/settings.py
@@ -163,9 +163,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAdminUser',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTTokenUserAuthentication',
     ),
 }
 
